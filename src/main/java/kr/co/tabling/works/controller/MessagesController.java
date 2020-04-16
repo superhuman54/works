c86c9310b60823b1d6942394f37505df5b374d2a
Jamie <superhuman54@gmail.com>
2020-04-16 23:38:38 +0900

    POST 메세지 생성.

--- src/main/java/kr/co/tabling/works/controller/MessagesController.java
+++ src/main/java/kr/co/tabling/works/controller/MessagesController.java
@@ -1,9 +1,15 @@
 package kr.co.tabling.works.controller;
 
+import kr.co.tabling.works.persist.entity.Message;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagesController {
 
 
+    @PostMapping()
+    public String post(Message body) {
+        return null;
+    }
 }
