9e5f843c6f7dc090371a8545f5204d8480cad549
Jamie <superhuman54@gmail.com>
2020-04-16 23:33:47 +0900

    GET 메세지 생성.

--- src/main/java/kr/co/tabling/works/controller/MessagesController.java
+++ src/main/java/kr/co/tabling/works/controller/MessagesController.java
@@ -1,9 +1,14 @@
 package kr.co.tabling.works.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagesController {
 
 
+    @GetMapping("/")
+    public String message() {
+        return null;
+    }
 }
