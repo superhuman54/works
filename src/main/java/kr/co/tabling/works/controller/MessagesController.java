6d7456e49bb433f386d2960a924344c05e6d9597
Jamie <superhuman54@gmail.com>
2020-04-17 00:34:41 +0900

    PUT 메세지 생성.

--- src/main/java/kr/co/tabling/works/controller/MessagesController.java
+++ src/main/java/kr/co/tabling/works/controller/MessagesController.java
@@ -2,14 +2,22 @@ package kr.co.tabling.works.controller;
 
 import kr.co.tabling.works.persist.entity.Message;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/messages")
 public class MessagesController {
 
 
     @PostMapping()
     public String post(Message body) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public String put(Message body) {
         return null;
     }
 }
